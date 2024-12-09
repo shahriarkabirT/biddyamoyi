318d23e072b352fe4e0401766b90fbcd2c1e68f4
Jumael Ahmed Khan <jumael99@gmail.com>
2024-12-09 19:13:34 +0600

    sidebar added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Header from './Header';
 import './styles/main.css';
+import RightSideBar from "./components/RightSideBar";
 
 function App() {
   return (
     <div>
       <Header/>
       <Navbar />
+        <RightSideBar />
       <Footer />
     </div>
   );
